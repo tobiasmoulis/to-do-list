b04d155d94aeb55f77e6f32cb6414f47bb770f93
OndraCedidla <cedidla.on.2020@skola.ssps.cz>
2023-09-19 17:02:01 +0200

    Uprava

--- toDoList/src/App.vue
+++ toDoList/src/App.vue
@@ -1,11 +1,21 @@
 <script setup>
+
 </script>
 
 <template>
-  <h1>ahoj</h1>
-  <h1>ahoj</h1>
-</template>
+  <div class="Page">
+    <div class="Header">
+      <h1>YOUR TO DO LIST</h1>
+    </div>
+  </div>
+  
+  
+ </template>
 
 <style lang="scss">
-h1{color: red}
+.Page{background-color: #08090B;height: 100%; width: 100%;}
+.Header{height: 40%; width: 100%};
+h1{color: #FDB669}
+h1{text-align: center; justify-content: center;}
+
 </style>
